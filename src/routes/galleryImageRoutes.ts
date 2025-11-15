517b77bba5da29d7096f9fe0493fcd58b0a020e5
danieljappe <danieljappe@icloud.com>
2025-11-15 21:09:14 +0100

    Increase image upload size from 5mb to 15mb

--- src/routes/galleryImageRoutes.ts
+++ src/routes/galleryImageRoutes.ts
@@ -284,7 +284,7 @@ const uploadGalleryImage: RequestHandler = async (req, res) => {
     console.log(`File received: ${req.file.originalname} (${req.file.size} bytes)`);
 
     // Additional file size check
-    if (req.file.size > 5 * 1024 * 1024) {
+    if (req.file.size > 10 * 1024 * 1024) {
       return res.status(400).json({ message: 'File size exceeds 5MB limit' });
     }
 
